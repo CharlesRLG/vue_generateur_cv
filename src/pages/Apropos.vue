6d5e0716e439b48c8798f26546ea574783f5cc78
Thomas Milton <perso.milton@gmail.com>
2022-02-27 16:05:46 +0100

    Maj des pages A PROPOS et MENTIONLEGALE

--- src/pages/Apropos.vue
+++ src/pages/Apropos.vue
@@ -1,5 +1,8 @@
   <template>
     <div class="Apropos">
         <h1>A propos</h1>
+
+        <h2>Qui sommmes-nous</h2>
+        <p>Lorem Elsass ipsum non blottkopf, Oberschaeffolsheim libero, tellus hopla lacus leo elementum aliquam Gal. jetz gehts los Christkindelsmärik réchime bissame nullam Wurschtsalad bredele mänele hopla gravida gal kougelhopf adipiscing ornare porta leo baeckeoffe amet, DNA, non leverwurscht flammekueche tristique chambon rhoncus nüdle libero. Hans sed gewurztraminer dui schnaps tchao bissame kuglopf kartoffelsalad Oberschaeffolsheim elit merci vielmols hopla sit ornare dolor vulputate ac Spätzle mollis ftomi! so condimentum Miss Dahlias libero, Yo dû. salu Salu bissame sit tellus sed geht's turpis, dignissim semper schpeck ullamcorper purus Racing. ac geïz eleifend Morbi météor Strasbourg Coopé de Truchtersheim hoplageiss amet hop und habitant sagittis wurscht auctor, ch'ai hopla vielmols, wie in, turpis Pfourtz ! Chulia Roberstau messti de Bischheim s'guelt yeuh. commodo Huguette et Pellentesque eget senectus pellentesque Richard Schirmeck quam, morbi barapli mamsell risus, amet picon bière sit schneck rossbolla Chulien Mauris knack varius placerat Kabinetpapier Verdammi Heineken quam. lotto-owe id rucksack suspendisse consectetur Salut bisamme id, Gal ! ante munster  knepfle Carola</p>
     </div>
   </template>
